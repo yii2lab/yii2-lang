d970d5a1fc355c8a7bc2e9f81fa82b60c718c5f6
Vitaliy <theyamshikov@yandex.ru>
2017-09-19 14:52:59 +0600

    fix

--- src/helpers/LangHelper.php
+++ src/helpers/LangHelper.php
@@ -1,29 +1,28 @@
 <?php
 namespace yii2module\lang\helpers;
 
+use Exception;
+use GuzzleHttp\Exception\ServerException;
 use Yii;
 use yii\helpers\ArrayHelper;
 use yii\helpers\FileHelper;
-use Exception;
+use yii\web\ServerErrorHttpException;
 use yii2lab\helpers\MenuHelper;
 
 class LangHelper {
-
-	public static function current()
-	{
+	
+	public static function current() {
 		$all = ArrayHelper::map(Yii::$app->lng->getAllLanguages(), 'code', 'title');
 		$lang = self::locale2lang(Yii::$app->language);
-		return $all[$lang];
+		return $all[ $lang ];
 	}
 	
-	public static function locale2lang($lang)
-	{
+	public static function locale2lang($lang) {
 		$langArr = explode('-', $lang);
 		return $langArr[0];
 	}
 	
-	public static function allForMenu()
-	{
+	public static function allForMenu() {
 		$all = ArrayHelper::map(Yii::$app->lng->getAllLanguages(), 'code', 'title');
 		$items = [];
 		foreach($all as $name => $title) {
@@ -35,73 +34,72 @@ class LangHelper {
 		}
 		return MenuHelper::gen($items);
 	}
-
-	public static function module($name, $message, $params = [], $language = null)
-	{
+	
+	public static function module($name, $message, $params = [], $language = null) {
 		$nameArr = explode('/', $name);
 		$moduleName = $nameArr[0];
 		$fileName = isset($nameArr[1]) ? $nameArr[1] : 'main';
 		if($moduleName == 'this' || empty($moduleName)) {
 			$moduleName = Yii::$app->controller->module->id;
 		}
-		$pathName = 'modules/'.$moduleName;
-		if(empty(Yii::$app->i18n->translations[$pathName.'/*'])) {
+		$pathName = 'modules/' . $moduleName;
+		if(empty(Yii::$app->i18n->translations[ $pathName . '/*' ])) {
 			self::registerModule($moduleName);
 		}
-		return Yii::t($pathName.'/'.$fileName, $message, $params, $language);
+		return Yii::t($pathName . '/' . $fileName, $message, $params, $language);
 	}
 	
-	private static function getLangFileNames($dir)
-	{
+	private static function getLangFileNames($dir) {
 		$dir = Yii::getAlias($dir);
 		/*if(!is_dir($dir)) {
 			return [];
 		}*/
 		$options['only'][] = '*.php';
 		$fileList = FileHelper::findFiles($dir, $options);
-		$fileList = array_map(function($file) {
+		$fileList = array_map(function ($file) {
 			return pathinfo($file, PATHINFO_FILENAME);
 		}, $fileList);
 		return $fileList;
 	}
 	
-	private static function genFileMap($moduleName, $dir)
-	{
+	private static function genFileMap($moduleName, $dir) {
 		$categoryList = self::getLangFileNames($dir);
 		if(empty($categoryList)) {
 			return [];
 		}
 		foreach($categoryList as $category) {
-			$map['modules/'.$moduleName.'/'.$category] = $category.'.php';
+			$map[ 'modules/' . $moduleName . '/' . $category ] = $category . '.php';
 		}
 		return $map;
 	}
 	
-	private static function registerModule($moduleName)
-	{
+	private static function registerModule($moduleName) {
 		$moduleClass = self::getModuleClass($moduleName);
 		$langDir = self::getModuleLangDir($moduleClass);
 		
 		if($langDir[0] == '@') {
 			try {
 				$dir = Yii::getAlias($langDir);
-			} catch(Exception $e) {}
+			} catch(Exception $e) {
+			}
 		} else {
 			$dir = ROOT_DIR . DS . $langDir;
 		}
-		if (is_dir($dir)) {
-			Yii::$app->i18n->translations['modules/'.$moduleName.'/*'] = [
-				'class'		  => 'yii\i18n\PhpMessageSource',
+		if(is_dir($dir)) {
+			Yii::$app->i18n->translations[ 'modules/' . $moduleName . '/*' ] = [
+				'class' => 'yii\i18n\PhpMessageSource',
 				'sourceLanguage' => 'xx-XX',
-				'basePath'	   => $dir,
-				'fileMap'		=> self::genFileMap($moduleName, $dir),
+				'basePath' => $dir,
+				'fileMap' => self::genFileMap($moduleName, $dir),
 			];
 		}
 	}
 	
-	private static function getModuleLangDir($moduleClass)
-	{
-		if (property_exists($moduleClass, 'langDir') && !empty($moduleClass::$langDir)) {
+	private static function getModuleLangDir($moduleClass) {
+		if(!class_exists($moduleClass)) {
+			Throw new ServerErrorHttpException('module_not_found');
+		}
+		if(property_exists($moduleClass, 'langDir') && !empty($moduleClass::$langDir)) {
 			$langDir = $moduleClass::$langDir;
 		} else {
 			$moduleClassFile = str_replace('\\', '/', $moduleClass);
@@ -109,17 +107,17 @@ class LangHelper {
 			$langDir = '@' . $moduleDir . '/messages';
 		}
 		return $langDir;
+		
+		
 	}
 	
-	private static function getModuleClass($moduleName)
-	{
+	private static function getModuleClass($moduleName) {
 		$moduleConfig = config('modules.' . $moduleName);
-		if (is_array($moduleConfig)) {
+		if(is_array($moduleConfig)) {
 			$moduleClass = $moduleConfig['class'];
 		} else {
 			$moduleClass = $moduleConfig;
 		}
 		return $moduleClass;
 	}
-	
 }
