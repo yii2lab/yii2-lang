1b4ab6f1e2902768c42f74867f220b29fe962030
Vitaliy <theyamshikov@yandex.ru>
2017-12-09 23:41:39 +0600

    func t deprecated

--- src/domain/helpers/Func.php
+++ src/domain/helpers/Func.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * @param       $category
+ * @param       $message
+ * @param array $params
+ * @param null  $language
+ *
+ * @return string
+ * @deprecated
+ */
 function t($category, $message, $params = [], $language = null) {
 	return \Yii::t($category, $message, $params, $language);
 }
