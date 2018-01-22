6c4f28aac98120e5d3e906fc5bdac8f9ba2365ef
Vitaliy <theyamshikov@yandex.ru>
2018-01-22 15:59:55 +0600

    add en lang

--- src/domain/enums/LanguageEnum.php
+++ src/domain/enums/LanguageEnum.php
@@ -7,6 +7,7 @@ use yii2lab\misc\enums\BaseEnum;
 class LanguageEnum extends BaseEnum {
 	
 	const RU = 'ru-RU';
+	const EN = 'en-UK';
 	const SOURCE = 'xx-XX';
 	
 	public static function code($locale) {
