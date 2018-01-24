5390f27facdef46945d441cc0f3be5a858f2e990
Vitaliy <theyamshikov@yandex.ru>
2018-01-24 12:30:26 +0600

    update test

--- tests/_config/main.php
+++ tests/_config/main.php
@@ -2,7 +2,7 @@
 
 use yii2module\lang\domain\enums\LanguageEnum;
 
-$config = require(ROOT_DIR . DS . TEST_APPLICATION_DIR . '//common/config/main.php');
+$config = require(ROOT_DIR . DS . TEST_APPLICATION_DIR .  DS . 'common/config/main.php');
 
 return \yii\helpers\ArrayHelper::merge($config, [
 	'language' => LanguageEnum::RU, // current Language
