84bcdfd6f26f9b33bd2b89327a7413ef46ad6795
Vitaliy <theyamshikov@yandex.ru>
2018-01-25 17:54:58 +0600

    update config

--- tests/_application/common/config/main.php
+++ tests/_application/common/config/main.php
@@ -1,13 +1,16 @@
 <?php
 
+use yii\helpers\ArrayHelper;
+use yii2lab\test\helpers\TestHelper;
 use yii2module\lang\domain\enums\LanguageEnum;
 
-$config = require(ROOT_DIR . DS . TEST_APPLICATION_DIR .  DS . 'common/config/main.php');
-
-return \yii\helpers\ArrayHelper::merge($config, [
+$config = [
 	'language' => LanguageEnum::RU, // current Language
 	'bootstrap' => ['log', 'language', 'queue'],
 	'components' => [
 		'language' => 'yii2module\lang\domain\components\Language',
 	],
-]);
+];
+
+$baseConfig = TestHelper::loadConfig('common/config/main.php');
+return ArrayHelper::merge($baseConfig, $config);
