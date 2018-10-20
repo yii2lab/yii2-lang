306c11c99a1a63c9afb21a9a0239bc2ab6ccb3e1
Vitaliy <theyamshikov@yandex.ru>
2018-10-21 00:02:19 +0600

    make autoload domain

--- src/domain/components/Language.php
+++ src/domain/components/Language.php
@@ -2,14 +2,17 @@
 
 namespace yii2module\lang\domain\components;
 
-use Yii;
 use yii\base\Component;
+use yii2lab\domain\helpers\DomainHelper;
 
 class Language extends Component
 {
 
 	public function init()
 	{
+		DomainHelper::forgeDomains([
+			'lang' => 'yii2module\lang\domain\Domain',
+		]);
 		\App::$domain->lang->language->initCurrent();
 		parent::init();
 	}
