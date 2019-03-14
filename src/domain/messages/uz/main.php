2fb51fb931dd7b4d72dd57dbe956392fdfec7bda
amubarak <amubarak@wooppay.com>
2019-03-14 16:57:12 +0600

    f

---
+++ src/domain/messages/uz/main.php
@@ -0,0 +1,13 @@
+﻿<?php
+
+return [
+	'title' => 'Тил',
+	'language_id' => 'Тилнинг ID',
+	'name' => 'Номи',
+	'language' => 'Тил',
+	'code'=>'Код',
+	'locale'=>'Маҳаллий',
+	'main_as_default'=>'Асосий тил',
+	
+	'title_languages' => 'Тиллар',
+];
